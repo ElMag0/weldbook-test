11731915e712b103b874bd2b6224086636127718
ilnur_ryazhapov <i.riazhapov@bars-open.ru>
2021-10-05 22:09:55 +0300

    routing module format code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: 'main', pathMatch: 'full'},
-  {path: 'main', component: MainComponent}
+  {
+    path: '',
+    redirectTo: 'main',
+    pathMatch: 'full'
+  },
+  {
+    path: 'main',
+    component: MainComponent
+  }
 ];
 
 @NgModule({
